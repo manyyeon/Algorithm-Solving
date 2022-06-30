fa0036b6d17f536674ca01d31b245ffb12bb1810
유다연 <udayeon@naver.com>
2022-06-30 03:09:26 +0000

    1316-그룹단어체커 풀이

--- main.py
+++ main.py
@@ -1,36 +1,21 @@
-text = input()
+import re
 
-stack = []
-arr = []
+splitWithTag = re.split('([<|>| ])', input())
 
-numLen = 0
-for i in range(len(text)):
-  if(text[i] >= '0' and text[i] <= '9'):
-    numLen += 1
-    if(i == (len(text)-1)):
-      j = i - numLen + 1
-      numLen = 0
-      num =""
-      while(j <= i):
-        num += text[j]
-        j += 1
-      stack.append(int(num))
-      arr.append(sum(stack))
-      stack = []
-      if(len(arr) == 1):
-        result = arr[0]
-      else:
-        result = 2*arr[0] - sum(arr)
-  elif(text[i] == '+' or text[i] == '-'):
-    j = i - numLen
-    numLen = 0
-    num =""
-    while(j < i):
-      num += text[j]
-      j += 1
-    stack.append(int(num))
-    if(text[i] == '-'):
-      arr.append(sum(stack))
-      stack = []
+newStr = ""
 
-print(result)
+i=0
+while(i < len(splitWithTag)):
+  if(splitWithTag[i] == '<'):
+    while(True):
+      if(splitWithTag[i] == '>'):
+        newStr += splitWithTag[i]
+        i+=1
+        break
+      newStr += splitWithTag[i]
+      i+=1
+  else:
+    newStr += "".join(reversed(splitWithTag[i]))
+    i+=1
+
+print(newStr)
