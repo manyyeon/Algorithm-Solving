2407bc88c25744672b175c430aed3ef75e08d96c
유다연 <udayeon@naver.com>
2022-06-30 08:42:20 +0000

    [이취코]greedy-3-숫자_카드_게임 풀이

--- 이취코/그리디/greedy-3-숫자_카드_게임.py
+++ 이취코/그리디/greedy-3-숫자_카드_게임.py
@@ -5,8 +5,4 @@ data = []
 for i in range(N):
   data.append(list(map(int, sys.stdin.readline().split())))
 
-minArr = []
-for i in range(N):
-  minArr.append(min(data[i]))
-
-print(max(minArr))
+print(data)
